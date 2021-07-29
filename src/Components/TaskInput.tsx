362c9493858d878e1e7bb0e0aa3890246c12011d
hubatruck <hubatruck2015@gmail.com>
2021-07-29 14:40:36 +0300

    TaskInput: Create component
    This component outsources the input field for creating and editing tasks
    into a separate file.
    There is the possibility to clear the data after submitting the form too.

---
+++ src/Components/TaskInput.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Button, Form, Input} from "antd";
+import {PlusOutlined} from "@ant-design/icons";
+
+type TaskInputProps = {
+    onSubmit: (values: { taskName: string }) => void;
+    clearOnSubmit?: boolean;
+}
+
+/// Note that useForm is a React Hooks that only works in functional component.
+export default function TaskInput(props: TaskInputProps) {
+    const [form] = Form.useForm();
+    return (
+        <Form
+            name="newTaskForm"
+            preserve={false}
+            onFinish={data => {
+                if (props.clearOnSubmit) {
+                    form.resetFields();
+                }
+                props.onSubmit(data);
+            }}
+            form={form}
+        >
+            <Form.Item name="taskName">
+                <Input.TextArea
+                    placeholder="Type task here..."
+                    allowClear
+                    style={{width: 400}}
+                    autoSize={{minRows: 2}}
+                    showCount
+                    maxLength={2000}
+                    onPressEnter={(event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+                        if (event.ctrlKey || event.shiftKey) {
+                            form.submit();
+                        }
+                    }}
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button
+                    type="primary"
+                    htmlType="submit"
+                    icon={<PlusOutlined/>}
+                    style={{marginTop: "5px"}}
+                > Add </Button>
+            </Form.Item>
+        </Form>);
+}
